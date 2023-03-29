b73145ef03993b4e805013c0872cb71bfc521bb0
Abdullah YILMAZ <h.abdullahyilmaz@hotmail.com>
2023-03-29 19:08:04 +0300

    Update alarm_telegram_sms.ino

--- alarm_telegram_sms/alarm_telegram_sms.ino
+++ alarm_telegram_sms/alarm_telegram_sms.ino
@@ -1,5 +1,5 @@
 /*******************************************************************
-    A Telegram App bot for ESP8266 that controls the 
+    A Telegram App bot for ESP8266 that controls the
     433 MHz home alarm system.
 
  *******************************************************************/
@@ -19,32 +19,42 @@
 #include <RCSwitch.h>// https://github.com/sui77/rc-switch
 #include <UniversalTelegramBot.h> //https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
 
-//Sirenin çalmaya devam edeceği süre (ms)
-#define sirenCalmaSuresi 30000
-
 //Mesaj gönderilecek ve aranacak telefon numarası
-const String PHONE = "+905555555555";
+const char* PHONE = "+905555555555";
 
 // Initialize Wifi connection to the router
-char ssid[] = "XXX";     // your network SSID (name)
-char password[] = "12345678"; // your network key
+const char* ssid = "XXX";     // your network SSID (name)
+const char* password = "12345678"; // your network key
 
 // Initialize Telegram BOT
-#define BOTtoken "xxx" // your Bot Token (Get from Botfather)
-String chat_id = "111111"; // your telegram user id (to chat with)
+const char* BOTtoken = "xxx:xxx"; // your Bot Token (Get from Botfather)
+const char* chat_id = "111111"; // your telegram user id (to chat with)
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
 
-RCSwitch rfReceiver = RCSwitch();
-
 //Checks for new messages every 1 second.
 int botRequestDelay = 5000; //1000; //[ms]
 unsigned long lastTimeBotRan;
-unsigned long sirenzaman=0;
+unsigned long sirenzaman = 0;
+
+RCSwitch rfReceiver = RCSwitch();
 
 const int rfReceiverPin = 18; //pin D2, works. Connect RFReceiver module to this pin. Power it from 3V3 (not from 5V because of risk of killing GPIO pin).
-const int rfTransmitPin = 10; //rf alıcısının pini
+const int rfTransmitterPin = 19;
+
+//Durum ledleri için değişken tanımlıyoruz
+#define sistemAktifLed 12
+#define sistemPasifLed 13
+
+//Siren için değişken tanımlıyoruz
+#define siren 2
+
+//Sirenin çalmaya devam edeceği süre (ms)
+#define sirenCalmaSuresi 30000
+
+// Variable to store text message
+String textMessage;
 
 typedef struct {
   int remoteID;
@@ -52,13 +62,6 @@ typedef struct {
   int group; // 0 - disarming; 1 - arming; 2 - reported 24h, 3 - reported when armed
   long lastTime;
 } device_struct;
-
-//Siren için değişken tanımlıyoruz
-#define siren 2
-
-//Durum ledleri için değişken tanımlıyoruz
-#define sistemAktifLed 12
-#define sistemPasifLed 13
 
 #define NUM_DEVICES 10 //<<< DİKKAT!! UNUTMA Cihaz sayısı
 device_struct devices[NUM_DEVICES] = { // add your device RF codes and names here
@@ -79,9 +82,6 @@ device_struct devices[NUM_DEVICES] = { // add your device RF codes and names her
   
 }; //make the last entry above without a column
 
-// Variable to store text message
-String textMessage;
-
 bool enable_system = true; //start armed
 bool enable_sniffing = false;
 
@@ -99,12 +99,13 @@ void parseRemote(int remoteID) {
     if (millis() - devices[currentDevice].lastTime > 1500) {
       devices[currentDevice].lastTime = millis();
       Serial.println(String(remoteID) + ": " + devices[currentDevice].remoteName);
-      
-      if ((devices[currentDevice].group == 0) && enable_system){
+
+      if ((devices[currentDevice].group == 0) && enable_system) {
         enable_system = false;
         digitalWrite(sistemAktifLed, LOW);
         digitalWrite(sistemPasifLed, HIGH);
         digitalWrite(siren, LOW);
+        Serial.println("Sistem pasifleştirildiği için siren susturuldu");
         bot.sendMessage(chat_id, devices[currentDevice].remoteName, "");
         //*****Sistem kumanda ile pasif duruma getirildiğinde sms göndermek için ayarlama yapıyoruz.(Bunu istemiyorsanız bu bölümü silin)
         String message = devices[currentDevice].remoteName;
@@ -112,41 +113,46 @@ void parseRemote(int remoteID) {
         //*****
       }
 
-      if (devices[currentDevice].group == 1){
+      if (devices[currentDevice].group == 1) {
         enable_system = true;
         digitalWrite(sistemAktifLed, HIGH);
         digitalWrite(sistemPasifLed, LOW);
         bot.sendMessage(chat_id, devices[currentDevice].remoteName, "");
       }
-        
-      if (devices[currentDevice].group == 2){
+
+      if (devices[currentDevice].group == 2) {
         bot.sendMessage(chat_id, devices[currentDevice].remoteName, "");
         String message = devices[currentDevice].remoteName;
         sendSMS(message);
       }
-      
-      if ((devices[currentDevice].group == 3) && enable_system){
+
+      if ((devices[currentDevice].group == 3) && enable_system) {
         sirenzaman = millis();
         digitalWrite(siren, HIGH);
+        Serial.println("Sensör tetiklendiği için siren çalmaya başladı");
         bot.sendMessage(chat_id, devices[currentDevice].remoteName, "");
         String message = devices[currentDevice].remoteName;
         sendSMS(message);
         delay(3000);
-        
-        Serial2.println("ATD+ "+PHONE+";");
+
+        Serial2.print("ATD+ ");
+        Serial2.print(PHONE);
+        Serial2.println(";");
         //Serial2.println("ATH");
-        // Set module to send SMS data to serial out upon receipt 
+        // Set module to send SMS data to serial out upon receipt
         Serial2.print("AT+CNMI=2,2,0,0,0\r");
       }
-      
-      if (devices[currentDevice].group == 4)
+
+      if (devices[currentDevice].group == 4) {
         digitalWrite(siren, LOW);
-      
+        Serial.println("Susturma tuşuna basıldığı için siren susturuldu");
+      }
+
     }
   } else {
     Serial.println(String(remoteID) + ": " + "bilinmeyen kod");
     if (enable_sniffing)
-      bot.sendMessage(chat_id, String(remoteID) + ": " + "bilinmeyen kod", ""); //use this to get the sensor id codes 
+      bot.sendMessage(chat_id, String(remoteID) + ": " + "bilinmeyen kod", ""); //use this to get the sensor id codes
   }
 }
 
@@ -154,7 +160,7 @@ void handleNewMessages(int numNewMessages) {
   //Serial.println("handleNewMessages");
   //Serial.println(String(numNewMessages));
 
-  for (int i=0; i<numNewMessages; i++) {
+  for (int i = 0; i < numNewMessages; i++) {
     //String chat_id = String(bot.messages[i].chat_id);
 
     //String from_name = bot.messages[i].from_name;
@@ -164,9 +170,9 @@ void handleNewMessages(int numNewMessages) {
       //bot.sendMessage(bot.messages[i].chat_id, "You are not authorized", "");
       return;
     }
-    
+
     String text = bot.messages[i].text;
-    
+
     if (text == "/aktif") {
       enable_system = true;
       digitalWrite(sistemAktifLed, HIGH);
@@ -180,15 +186,15 @@ void handleNewMessages(int numNewMessages) {
       digitalWrite(sistemPasifLed, HIGH);
       bot.sendMessage(chat_id, "Sistem Devre Dışı", "");
     }
-    
+
     else if (text == "/durum") {
-      if(enable_system){
+      if (enable_system) {
         bot.sendMessage(chat_id, "Durum: Sistem Aktif", "");
       } else {
         bot.sendMessage(chat_id, "Durum: Sistem Devre Dışı", "");
       }
     }
-    
+
     else if (text == "/liste") {
       String list = "Cihaz kodları, isimleri ve grupları:\n";
       for (int currentDevice = 0; currentDevice < NUM_DEVICES; currentDevice++ ) {
@@ -196,10 +202,10 @@ void handleNewMessages(int numNewMessages) {
       }
       bot.sendMessage(chat_id, list, "");
     }
-    
+
     else if (text == "/dinle") {
       enable_sniffing = !enable_sniffing;
-      if(enable_sniffing){
+      if (enable_sniffing) {
         bot.sendMessage(chat_id, "RF algılama açık", "");
       } else {
         bot.sendMessage(chat_id, "RF algılama kapalı", "");
@@ -220,34 +226,96 @@ void handleNewMessages(int numNewMessages) {
   }
 }
 
+// Function that sends SMS
+void sendSMS(String message) {
+  // AT command to set SIM900 to SMS mode
+  Serial2.println("AT+CMGF=1\r\n");
+  delay(100);
+
+  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
+  Serial2.print("AT+CMGS=\"");
+  Serial2.print(PHONE);
+  Serial2.println("\"\r\n");
+  delay(500);
+
+  // Send the SMS
+  Serial2.println(message);
+  delay(500);
+
+  // End AT command with a ^Z, ASCII code 26
+  Serial2.println((char)26);
+}
+
+void SMScontrol() {
+
+  //Sms ile sistemi açmak için ayarlıyoruz
+  if ((textMessage.indexOf("#AC") >= 0) && !enable_system) { //Gönderilecek sms içeriği # dahil
+    enable_system = true;
+    digitalWrite(sistemAktifLed, HIGH);
+    digitalWrite(sistemPasifLed, LOW);
+    String message = "Sistem Sms ile Aktif Duruma Getirildi";
+    sendSMS(message);
+    textMessage = "";
+  }
+
+  //Sms ile sistemi kapatmak için ayarlıyoruz
+  if ((textMessage.indexOf("#KAPAT") >= 0) && enable_system) { //Gönderilecek sms içeriği # dahil
+    enable_system = false;
+    digitalWrite(sistemAktifLed, LOW);
+    digitalWrite(sistemPasifLed, HIGH);
+    digitalWrite(siren, LOW);
+    Serial.println("Sms ile sistem pasifleştirildiği için siren susturuldu");
+    String message = "Sistem Sms ile Pasif Duruma Getirildi";
+    sendSMS(message);
+    textMessage = "";
+  }
+
+  //Sms ile sistemin durumunu sorguluyoruz
+  if (textMessage.indexOf("#DURUM") >= 0) { //Gönderilecek sms içeriği # dahil
+    if (enable_system) {
+      String message = "Durum: Sistem Aktif";
+      sendSMS(message);
+      textMessage = "";
+    } else {
+      String message = "Durum: Sistem Devre Disi";
+      sendSMS(message);
+      textMessage = "";
+    }
+  }
+
+  //Sms ile 1. kepenke komut gönderiyoruz
+  if (textMessage.indexOf("#KEPENK1") >= 0) { //Gönderilecek sms içeriği # dahil
+    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
+    String message = "SMS iLE 1. KEPENKE KOMUT GONDERiLDi.";
+    sendSMS(message);
+    delay(1000);
+    textMessage = "";
+  }
+
+  //Sms ile 2. kepenke komut gönderiyoruz
+  if (textMessage.indexOf("#KEPENK2") >= 0) { //Gönderilecek sms içeriği # dahil
+    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
+    String message = "SMS iLE 2. KEPENKE KOMUT GONDERiLDi.";
+    sendSMS(message);
+    delay(1000);
+    textMessage = "";
+  }
+
+  //Sms ile 3. kepenke komut gönderiyoruz
+  if (textMessage.indexOf("#KEPENK3") >= 0) { //Gönderilecek sms içeriği # dahil
+    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
+    String message = "SMS iLE 3. KEPENKE KOMUT GONDERiLDi.";
+    sendSMS(message);
+    delay(1000);
+    textMessage = "";
+  }
+
+}
+
 void setup() {
   //Esp32 ile SIM800 seri haberleşmesi başlatılıyor
   Serial.begin(115200);
   Serial2.begin(9600);
-
-  rfReceiver.enableReceive(rfReceiverPin);
-  rfReceiver.enableTransmit(rfTransmitPin);
-
-  // Sim800ün çalışması için 5 saniye bekliyoruz(Bunu yapmazsak aşağıdaki sim800ü sms moduna alma işlemi gerçekleşmez)
-  delay(5000);
-
-  // AT command to set SIM900 to SMS mode
-  Serial2.print("AT+CMGF=1\r"); 
-  delay(100);
-  // Set module to send SMS data to serial out upon receipt 
-  Serial2.print("AT+CNMI=2,2,0,0,0\r");
-  delay(100);
-  
-  //Siren pininin çıkış pini olduğunu belirtiyoruz
-  pinMode (siren, OUTPUT);
-
-  //Durum ledlerini ayarlıyoruz
-  pinMode (sistemAktifLed, OUTPUT);
-  pinMode (sistemPasifLed, OUTPUT);
-  digitalWrite(sistemAktifLed, HIGH);
-  digitalWrite(sistemPasifLed, LOW);
-
-  delay(10);
 
   // This is the simplest way of getting this working
   // if you are passing sensitive information, or controlling
@@ -273,97 +341,51 @@ void setup() {
 
   Serial.println("");
   Serial.println("WiFi bağlandı");
-  Serial.print("IP adres: ");
+  Serial.print("IP adresi: ");
   Serial.println(WiFi.localIP());
 
+  rfReceiver.enableReceive(rfReceiverPin);
+  rfReceiver.enableTransmit(rfTransmitterPin);
 
-  // Sim800ün çalışması için 4 saniye bekliyoruz(Bunu yapmazsak aşağıdaki sim800ü sms moduna alma işlemi gerçekleşmez)
-  delay(4000);
+  //Siren pininin çıkış pini olduğunu belirtiyoruz
+  pinMode (siren, OUTPUT);
+
+  //Durum ledlerini ayarlıyoruz
+  pinMode (sistemAktifLed, OUTPUT);
+  pinMode (sistemPasifLed, OUTPUT);
+
+  digitalWrite(sistemAktifLed, HIGH);
+  digitalWrite(sistemPasifLed, LOW);
+
+  // Sim800ün çalışması için 5 saniye bekliyoruz(Bunu yapmazsak aşağıdaki sim800ü sms moduna alma işlemi gerçekleşmez)
+  delay(5000);
 
   // AT command to set SIM900 to SMS mode
-  Serial2.print("AT+CMGF=1\r"); 
+  Serial2.print("AT+CMGF=1\r");
   delay(100);
-  // Set module to send SMS data to serial out upon receipt 
+  // Set module to send SMS data to serial out upon receipt
   Serial2.print("AT+CNMI=2,2,0,0,0\r");
   delay(100);
 
   bot.sendMessage(chat_id, "Sistem Başlatıldı", "");
+  Serial.println("Sistem Başlatıldı.");
 }
 
 void loop() {
   //Sim800den seri monitöre komut göndermek için ayarlama yapıyoruz. Bu arada textMessage değişkenini sim800den gelen komut ile sabitliyoruz
-  if(Serial2.available()>0){
+  if (Serial2.available() > 0) {
     textMessage = Serial2.readString();
     Serial.print(textMessage);
   }
 
   //Seri monitörden sim800e komut göndermek için ayarlıyoruz. Bu kod test aşamasında gerekli. Daha sonra kaldıralım
-  if(Serial.available()) {
-    Serial2.write(Serial.read()); 
+  if (Serial.available()) {
+    Serial2.write(Serial.read());
   }
 
-  //Sms ile sistemi açmak için ayarlıyoruz
-  if((textMessage.indexOf("#AC")>=0) && !enable_system){ //Gönderilecek sms içeriği # dahil
-    enable_system = true;
-    digitalWrite(sistemAktifLed, HIGH);
-    digitalWrite(sistemPasifLed, LOW);
-    String message = "Sistem Sms ile Aktif Duruma Getirildi";
-    sendSMS(message);
-    textMessage = "";   
-  }
+  SMScontrol();
 
-  //Sms ile sistemi kapatmak için ayarlıyoruz
-  if((textMessage.indexOf("#KAPAT")>=0) && enable_system){ //Gönderilecek sms içeriği # dahil
-    enable_system = false;
-    digitalWrite(sistemAktifLed, LOW);
-    digitalWrite(sistemPasifLed, HIGH);
-    digitalWrite(siren, LOW);
-    String message = "Sistem Sms ile Pasif Duruma Getirildi";
-    sendSMS(message);
-    textMessage = ""; 
-  }
-
-  //Sms ile sistemin durumunu sorguluyoruz
-  if(textMessage.indexOf("#DURUM")>=0){ //Gönderilecek sms içeriği # dahil
-    if(enable_system){
-      String message = "Durum: Sistem Aktif";
-      sendSMS(message);
-      textMessage = "";
-    } else {
-      String message = "Durum: Sistem Devre Disi";
-      sendSMS(message);
-      textMessage = "";
-    }
-  }
-
-  //Sms ile 1. kepenke komut gönderiyoruz
-  if(textMessage.indexOf("#KEPENK1")>=0){ //Gönderilecek sms içeriği # dahil
-    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
-    String message = "SMS iLE 1. KEPENKE KOMUT GONDERiLDi.";
-    sendSMS(message);
-    delay(1000);
-    textMessage = "";
-  }
-
-  //Sms ile 2. kepenke komut gönderiyoruz
-  if(textMessage.indexOf("#KEPENK2")>=0){ //Gönderilecek sms içeriği # dahil
-    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
-    String message = "SMS iLE 2. KEPENKE KOMUT GONDERiLDi.";
-    sendSMS(message);
-    delay(1000);
-    textMessage = "";
-  }
-
-  //Sms ile 3. kepenke komut gönderiyoruz
-  if(textMessage.indexOf("#KEPENK3")>=0){ //Gönderilecek sms içeriği # dahil
-    rfReceiver.send("000"); //Kepenk için gönderilecek rf kodunu girin
-    String message = "SMS iLE 3. KEPENKE KOMUT GONDERiLDi.";
-    sendSMS(message);
-    delay(1000);
-    textMessage = "";
-  }
-
-  if (millis() - sirenzaman > sirenCalmaSuresi){
+  if (millis() - sirenzaman > sirenCalmaSuresi) {
     digitalWrite(siren, LOW);
   }
 
@@ -380,28 +402,10 @@ void loop() {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
     //while(numNewMessages) { //AMA commented this out, check only one message per loop cycle
-      //Serial.println("got response");
-      handleNewMessages(numNewMessages);
-      //numNewMessages = bot.getUpdates(bot.last_message_received + 1); //AMA commented this out, process only one message per turn
+    //Serial.println("got response");
+    handleNewMessages(numNewMessages);
+    //numNewMessages = bot.getUpdates(bot.last_message_received + 1); //AMA commented this out, process only one message per turn
     //}
     lastTimeBotRan = millis();
   }
-}
-
-// Function that sends SMS
-void sendSMS(String message){
-  // AT command to set SIM900 to SMS mode
-  Serial2.println("AT+CMGF=1\r\n"); 
-  delay(100);
-
-  // USE INTERNATIONAL FORMAT CODE FOR MOBILE NUMBERS
-  Serial2.println("AT+CMGS=\""+PHONE+"\"\r\n"); 
-  delay(500);
-  
-  // Send the SMS
-  Serial2.println(message); 
-  delay(500);
-
-  // End AT command with a ^Z, ASCII code 26
-  Serial2.println((char)26);
 }
